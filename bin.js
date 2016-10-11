178bccfaedad5341188c79650e67cbc888782ef6
syzer3@gmail.com <syzer3@gmail.com>
2016-10-11 09:56:43 +0200

    bla... bla.. bla..

--- bin.js
+++ bin.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-'use strict'
 
 const errMsg = require('./errorMsg')
 const argv = require('yargs')
@@ -7,12 +6,12 @@ const argv = require('yargs')
     .option('c', {
         alias: 'cols',
         default: 80,
-        describe: 'how many cols in terminal'
+        describe: 'Number of columns in terminal'
     })
     .option('r', {
         alias: 'ratio',
         default: 1,
-        describe: 'ratio try 0.5 to flatten image\n and 2 to lengthen image'
+        describe: 'Aspect ratio. Try 0.5 to flatten image\n and 2 to lengthen image'
     })
     .demand(1)
     .argv
